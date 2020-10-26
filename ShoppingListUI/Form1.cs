031d65f9222bec53cdcca6e3700291e00c79a030
emanuel1996 <emanuel_sb_96@yahoo.com>
2020-10-26 15:27:40 +0200

    update

--- ShoppingListUI/Form1.cs
+++ ShoppingListUI/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,21 +26,34 @@ namespace ShoppingListUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            List<Item> shoppingList = new List<Item>()
+        List<Item> shoppingList = new List<Item>()
             {
                 new Item{ItemName = "Bread", Price = 4.00M},
                 new Item{ItemName = "Cheese", Price = 10.00M},
                 new Item{ItemName = "Eggs", Price = 7.00M},
                 new Item{ItemName = "Bacon", Price = 15.00M},
             };
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
             checkedShoppingList.DisplayMember = "Name - Price";
             checkedShoppingList.DataSource = shoppingList;
+
+            lblTotal.Text = CalculateTotal();
+        }
+
+        private string CalculateTotal()
+        {
+            decimal total = 0;
+            foreach (var item in shoppingList)
+            {
+                total += item.Price;
+            }
+
+            return total.ToString();
         }
     }
 }
